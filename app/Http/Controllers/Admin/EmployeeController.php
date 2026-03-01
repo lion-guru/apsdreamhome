c8167549a7f58bbf2047b8d18c339ab6f58a0510
lion-guru <techguruabhay@gmail.com>
2026-03-01 17:28:18 +0530

    Fix syntax errors in SecurityAudit.php and Admin/EmployeeController.php

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -39,7 +39,7 @@ class EmployeeController extends AdminController
      */
     public function create()
     {
-        $employeeModel = $this->model(->with(['getRoles', 'getDepartments', 'getEmployeeByEmail', 'createEmployee', 'getEmployeeById', 'updateEmployee', 'deleteEmployee', 'offboardEmployee'])'Employee');
+        $employeeModel = $this->model('Employee');
         $roles = $employeeModel->getRoles();
         $departments = $employeeModel->getDepartments();
 
