fba3384fd09386b5a6b3165667dab3f28880b514
lion-guru <techguruabhay@gmail.com>
2026-03-06 18:24:55 +0530

    [Auto-Fix] Systematic Organization: Fixed MVC structure and removed all duplicate files

--- app/core/App_fix.php
+++
@@ -1,5 +0,0 @@
-// Property routes
-        } elseif ($uri === "/properties") {
-            return $this->loadController("Property\\PropertyController", "index");
-        } elseif (preg_match('/^\/properties\/(\d+)$/', $uri, $matches)) {
-            return $this->loadController("HomeController", "propertyDetail", [$matches[1]]);
