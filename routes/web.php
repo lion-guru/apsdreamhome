d4e3df704b3addb707411aed2947a586e04ee569
lion-guru <techguruabhay@gmail.com>
2026-03-23 18:35:43 +0530

    [Senior-Developer] Cleaned Up Duplicate Files - Removed 4 duplicate files, cleaned routes and controller, only unified platform remains

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,6 @@ $router->post('/senior-developer/execute', 'App\\Http\\Controllers\\AIController
 $router->get('/senior-developer/logs', 'App\\Http\\Controllers\\AIController@seniorDeveloperLogs');
 $router->get('/senior-developer/monitor', 'App\\Http\\Controllers\\AIController@seniorDeveloperMonitor');
 $router->get('/senior-developer/dashboard', 'App\\Http\\Controllers\\AIController@seniorDeveloperDashboard');
-$router->get('/senior-developer/chat', 'App\\Http\\Controllers\\AIController@seniorDeveloperChat');
-$router->get('/senior-developer/dashboard-v2', 'App\\Http\\Controllers\\AIController@seniorDeveloperDashboardV2');
-$router->get('/senior-developer/code-editor-simple', 'App\\Http\\Controllers\\AIController@seniorDeveloperCodeEditorSimple');
 $router->get('/senior-developer/unified', 'App\\Http\\Controllers\\AIController@seniorDeveloperUnified');
 $router->post('/senior-developer/save-code', 'App\\Http\\Controllers\\AIController@saveCode');
 $router->post('/senior-developer/run-code', 'App\\Http\\Controllers\\AIController@runCode');
